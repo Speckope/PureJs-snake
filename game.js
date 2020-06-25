3db590566935ba1a4d4e5853f9081abe6e79698b
Speckope <Speckope@gmail.com>
2020-06-25 20:39:44 +0200

    made  html, style.css, and base game and snake.js

---
+++ game.js
@@ -0,0 +1,39 @@
+import { SNAKE_SPEED } from './snake.js';
+
+let lastRenderTime = 0;
+// curTime is exact time when our function runs  border: .25vmin solid black;
+// we want to be recalling this function immidiately so we get another loop.
+// This function will loop continously
+function main(curTime) {
+  const secondsSinceLastRender = (curTime - lastRenderTime) / 1000;
+  // It tells us when we can render next frame
+  // we r requesting a frame to animate a game
+  // -----------
+  //   "Metoda Window.requestAnimationFrame() informuje
+  //   przeglądarkę o zamiarze wykonania animacji i żąda od
+  //   przeglądarki wywołania określonej funkcji w celu
+  //   odświeżenia animacji przed następnym odmalowaniem.
+  //   Argumentem metody jest funkcja (callback) do
+  //   wywołania przed następnym odmalowaniem
+  //   (odświeżeniem kanwy)."
+  // --------------
+  window.requestAnimationFrame(main);
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  console.log('Render');
+  lastRenderTime = curTime;
+
+  //   Games are usually broken into two different steps
+  //   update() updates all of the logic for our game
+  // !!! 1. Update will move snake to position, update food, life etc
+  update();
+  //   draw() or render() draws  everything on  the screen based on update loop
+  // !!! 2.Draw will actually render logic
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {}
+
+function draw() {}
